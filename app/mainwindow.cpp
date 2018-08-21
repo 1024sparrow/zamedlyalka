d50f95e9e7d0599cdc6efde73a67f2f41631d6a6
1024sparrow <1024sparrow@gmail.com>
2018-08-21 05:20:46 +0300

    123

--- app/mainwindow.cpp
+++ app/mainwindow.cpp
@@ -1,7 +1,29 @@
 #include "mainwindow.h"
 
+#include <QHBoxLayout>
+#include <QPushButton>
+#include <QStackedWidget>
+
 MainWindow::MainWindow(QWidget *parent)
     :QWidget(parent)
 {
+    QBoxLayout *mainLay = new QHBoxLayout(this);
+    QWidget *leftsidePanel = new QWidget(this);
+    {
+        QPushButton *bn1 = new QPushButton("generator", this);
+        QBoxLayout *lay = new QVBoxLayout(leftsidePanel);
+        lay->addWidget(bn1);
+    }
+    QStackedWidget *gameplayArea = new QStackedWidget(this);
+    {
+        //
+    }
+    mainLay->addWidget(leftsidePanel);
+    mainLay->addWidget(gameplayArea);
 
+}
+
+void MainWindow::onBn1Clicked()
+{
+    //
 }
--- app/mainwindow.h
+++ app/mainwindow.h
@@ -2,11 +2,19 @@
 #define MAINWINDOW_H
 
 #include <QWidget>
+class QStackedWidget;
 
 class MainWindow : public QWidget
 {
 public:
     MainWindow(QWidget *parent = 0);
+
+private slots:
+    void onBn1Clicked();
+
+private:
+    QStackedWidget *gameplayArea;
+
 };
 
 #endif // MAINWINDOW_H
