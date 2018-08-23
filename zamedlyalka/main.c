a8fd003f5c975b42d3fa4701be1163b8a18c8df6
1024sparrow <1024sparrow@gmail.com>
2018-08-23 12:37:49 +0300

    123

--- zamedlyalka/main.c
+++ zamedlyalka/main.c
@@ -1,10 +1,29 @@
-#include <stdio.h>
-#include <string.h>
-//#include <stdlib.h>
+#include <stdio.h> // printf()
+#include <string.h> // strcmp()
+//#include <stdlib.h> // system()
+#include <sys/types.h>
+#include <unistd.h> // pipe(), fork()
+
+#define ERR(T) {fprintf(stderr, T);return 1;}
+
 int main()
 {
     printf("1234\n");
     //system("stty raw");//seting the terminal in raw mode
+    
+    int fdPipe[2];
+    if (pipe(fdPipe) < 0)
+        ERR("не смог открыть канал (внутрення ошибка)")
+
+    pid_t child = fork();
+    if (child < 0)
+        ERR("не смог породить дочерний процесс (внутренняя ошибка)")
+    else if (child == 0) // дочерний процесс
+    {
+    }
+    else // родительский процесс
+    {
+    }
 
     char command[1024];
     while (strcmp(fgets(command, 1024, stdin), "quit\n") != 0)
