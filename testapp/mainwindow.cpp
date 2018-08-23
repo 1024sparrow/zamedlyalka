3d12fbd52195b325e2d9f9d94d2970c2fc031cb5
1024sparrow <1024sparrow@gmail.com>
2018-08-23 12:00:33 +0300

    123

--- testapp/mainwindow.cpp
+++ testapp/mainwindow.cpp
@@ -1,10 +1,12 @@
 #include "mainwindow.h"
 #include "test1.h"
+#include <stdio.h>
 
 #include <QHBoxLayout>
 #include <QPushButton>
 #include <QStackedWidget>
 #include <QKeyEvent>
+#include <QTextStream>
 #include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
@@ -19,11 +21,12 @@ MainWindow::MainWindow(QWidget *parent)
     connect(process, SIGNAL(started()), this, SLOT(started()));
     connect(process, SIGNAL(stateChanged(QProcess::ProcessState)), this, SLOT(stateChanged(QProcess::ProcessState)));
     process->start("./zamedlyalka");
+   // process->start("./zamedlyalka__mplayer_bash");
     if (!process->waitForStarted(3000))
     {
         qDebug()<<"can't start";
     }
-    qDebug()<<"started";
+    qDebug()<<"__khgkjhgkjhgjhg";
     // -----------
     QBoxLayout *mainLay = new QHBoxLayout(this);
     QWidget *leftsidePanel = new QWidget(this);
@@ -48,79 +51,88 @@ void MainWindow::onBn1Clicked()
 
 void MainWindow::onProcess()
 {
-    qDebug()<<"onProcess";
+    printf("onProcess (about to close)\n");
 }
 
 void MainWindow::errorOccurred(QProcess::ProcessError error)
 {
-    qDebug()<<"errorOccurred";
+    printf("errorOccurred\n");
 }
 
 void MainWindow::finished(int exitCode, QProcess::ExitStatus exitStatus)
 {
-    qDebug()<<"finished";
+    printf("finished\n");
+    exit(0);
 }
 
 void MainWindow::readyReadStandardError()
 {
-    qDebug()<<"readyReadStandardError";
+    printf("readyReadStandardError\n");
+    QByteArray ba = process->readAllStandardError();
+    printf(ba.toStdString().c_str());
 }
 
 void MainWindow::readyReadStandardOutput()
 {
-    //qDebug()<<"readyReadStandardOutput";
-    qDebug()<<process->readAllStandardOutput();
+    printf("readyReadStandardOutput\n");
+    QByteArray ba = process->readAllStandardOutput();
+    printf(ba.toStdString().c_str());
 }
 
 void MainWindow::started()
 {
-    qDebug()<<"started";
+    printf("started\n");
 }
 
 void MainWindow::stateChanged(QProcess::ProcessState newState)
 {
-    qDebug()<<"stateChanged";
+    //printf("stateChanged\n");
 }
-
-int transformKeyCode(int p)
+const char * transformKeyCode(int p)
 {
     qDebug()<<"qt key code: "<<p;
     switch (p){
-    case 16777216: return 27; // <ESC>
-    case 81: return 113; // q
-    case 87: return 119; // w
-    case 69: return 101; // e
-    case 82: return 114; // r
-    case 84: return 116; // t
-    case 89: return 121; // y
-    case 85: return 117; // u
-    case 73: return 105; // i
-    case 79: return 111; // o
-    case 80: return 112; // p
-    case 65: return 97; // a
-    case 83: return 115; // s
-    case 68: return 100; // d
-    case 70: return 102; // f
-    case 71: return 103; // g
-    case 72: return 104; // h
-    case 74: return 106; // j
-    case 75: return 107; // k
-    case 76: return 108; // l
-    case 90: return 122; // z
-    case 88: return 120; // x
-    case 67: return 99; // c
-    case 86: return 118; // v
-    case 66: return 98; // b
-    case 78: return 110; // n
-    case 77: return 109; // m
+    case 16777216: return "quit\n"; // <ESC>
+    case 81: return "q\n"; // q
+    case 87: return "w\n"; // w
+    case 69: return "e\n"; // e
+    case 82: return "r\n"; // r
+    case 84: return "t\n"; // t
+    case 89: return "y\n"; // y
+    case 85: return "u\n"; // u
+    case 73: return "i\n"; // i
+    case 79: return "o\n"; // o
+    case 80: return "p\n"; // p
+    case 65: return "a\n"; // a
+    case 83: return "s\n"; // s
+    case 68: return "d\n"; // d
+    case 70: return "f\n"; // f
+    case 71: return "g\n"; // g
+    case 72: return "h\n"; // h
+    case 74: return "j\n"; // j
+    case 75: return "k\n"; // k
+    case 76: return "l\n"; // l
+    case 90: return "z\n"; // z
+    case 88: return "x\n"; // x
+    case 67: return "c\n"; // c
+    case 86: return "v\n"; // v
+    case 66: return "b\n"; // b
+    case 78: return "n\n"; // n
+    case 77: return "m\n"; // m
+    default: return NULL;
     }
 }
 
 void MainWindow::keyPressEvent(QKeyEvent *event)
 {
-    qDebug()<<"key pressed: "<<event->text();
-    int k = transformKeyCode(event->key());
-    process->write("q");
+    const char * k = transformKeyCode(event->key());
+    //printf("key pressed: %s(%i)\n", event->text().toStdString().c_str(), k);
+    //printf("----key pressed: %s\n", k);
+    if (k != NULL)
+        process->write(k);
+    //QTextStream stream(process);
+    //stream << "qwer";
+    //stream.setDevice(process);
 //    process->closeWriteChannel();
     //if (k)
 }
--- zamedlyalka/main.c
+++ zamedlyalka/main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 //#include <stdlib.h>
 int main()
 {
+    printf("1234\n");
     //system("stty raw");//seting the terminal in raw mode
-    char a;// = getch();
-    while ((a = getchar()) != 27)
+
+    char command[1024];
+    while (strcmp(fgets(command, 1024, stdin), "quit\n") != 0)
     {
-        printf("%c(%i)\n", a, a);
+        printf("command: %s", command);
     }
-    //system("stty cooked"); // возвращаем консоль в нормальный режим работы
-    //printf("%i", a);
     return 0;
+
+    //system("stty cooked"); // возвращаем консоль в нормальный режим работы
 }
